d108a31549d595cb303afb77cab7032389b707cc
Kurtsev Dmitriy <boman@bk.ru>
2023-04-12 13:55:27 +0500

    add test and modify

--- src/test/java/OrderingCardDeliveryElementsTest.java
+++ src/test/java/OrderingCardDeliveryElementsTest.java
@@ -1,14 +1,14 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 class OrderingCardDeliveryElementsTest {
@@ -20,28 +20,74 @@ class OrderingCardDeliveryElementsTest {
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
     }
 
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
     @Test
     void shouldTestPositiveCityEk() {
         $("[data-test-id=city] input").setValue("Ек");
         $(byText("Екатеринбург")).click();
-        String dateDelivery = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id=date] input").setValue(dateDelivery);
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Иванов Петр");
         $("[data-test-id=phone] input").setValue("+70000000000");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
+
     @Test
     void shouldTestPositiveData7() {
         $("[data-test-id=city] input").setValue("Ек");
         $(byText("Екатеринбург")).click();
-        String dateDelivery = LocalDate.now().plusDays(7).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id=date] input").setValue(dateDelivery);
+        $(".icon_name_calendar").click();
+        //$x("/html/body/div[2]/div/div/div/div/div/div/div[4]").click();
+        //$(byText("Май 2023")).shouldBe(Condition.visible);
+        $x("/html/body/div[2]/div/div/div/div/div/table/tbody/tr[5]/td[3]").click();
+        //$(byText("19")).click();
         $("[data-test-id=name] input").setValue("Иванов Петр");
         $("[data-test-id=phone] input").setValue("+70000000000");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + "19.04.2023"), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+    }
+    @Test
+    void shouldTestPositiveDataNextMonth() {
+        $("[data-test-id=city] input").setValue("Ек");
+        $(byText("Екатеринбург")).click();
+        $(".icon_name_calendar").click();
+        $x("/html/body/div[2]/div/div/div/div/div/div/div[4]").click();
+        $(byText("Май 2023")).shouldBe(Condition.visible);
+        //$x("/html/body/div[2]/div/div/div/div/div/table/tbody/tr[5]/td[3]").click();
+        $(byText("19")).click();
+        $("[data-test-id=name] input").setValue("Иванов Петр");
+        $("[data-test-id=phone] input").setValue("+70000000000");
+        $("[data-test-id=agreement]").click();
+        $(byText("Забронировать")).click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + "19.05.2023"), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+    }
+    @Test
+    void shouldTestPositiveDataNextYears() {
+        $("[data-test-id=city] input").setValue("Ек");
+        $(byText("Екатеринбург")).click();
+        $(".icon_name_calendar").click();
+        $x("/html/body/div[2]/div/div/div/div/div/div/div[3]").click();
+        $x("/html/body/div[2]/div/div/div/div/div/div/div[4]").click();
+        $(byText("Май 2024")).shouldBe(Condition.visible);
+        $(byText("20")).click();
+        $("[data-test-id=name] input").setValue("Иванов Петр");
+        $("[data-test-id=phone] input").setValue("+70000000000");
+        $("[data-test-id=agreement]").click();
+        $(byText("Забронировать")).click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + "20.05.2024"), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
--- src/test/java/OrderingCardDeliveryTest.java
+++ src/test/java/OrderingCardDeliveryTest.java
@@ -1,14 +1,15 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 class OrderingCardDeliveryTest {
@@ -20,50 +21,59 @@ class OrderingCardDeliveryTest {
         $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
     }
 
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
     @Test
     void shouldTestPositive() {
         $("[data-test-id=city] input").setValue("Екатеринбург");
-        String dateDelivery = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id=date] input").setValue(dateDelivery);
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Иванов Петр");
         $("[data-test-id=phone] input").setValue("+70000000000");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
-        }
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+    }
+
     @Test
     void shouldTestNegativeCity() {
         $("[data-test-id=city] input").setValue("Геленджик");
-        String dateDelivery = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id=date] input").setValue(dateDelivery);
+        String planningDate = generateDate(5, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Иванов Петр");
         $("[data-test-id=phone] input").setValue("+70000000000");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $(withText("Доставка в выбранный город недоступна")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave(text("Доставка в выбранный город недоступна"));
     }
+
     @Test
     void shouldTestEmptyCity() {
-        $("[data-test-id=city] input").setValue("");
-        String dateDelivery = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id=date] input").setValue(dateDelivery);
+        String planningDate = generateDate(5, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Иванов Петр");
         $("[data-test-id=phone] input").setValue("+70000000000");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $("[data-test-id=city].input_invalid .input__sub").shouldBe(text("Поле обязательно для заполнения"));
+        $("[data-test-id=city].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
     }
+
     @Test
     void shouldTestNegativeDate() {
         $("[data-test-id=city] input").setValue("Екатеринбург");
-        String dateDelivery = LocalDate.now().plusDays(2).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id=date] input").setValue(dateDelivery);
+        String planningDate = generateDate(1, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Иванов Петр");
         $("[data-test-id=phone] input").setValue("+70000000000");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $("[data-test-id=date] .input_invalid .input__sub").shouldBe(text("Заказ на выбранную дату невозможен"));
+        $("[data-test-id=date] .input_invalid .input__sub").shouldHave(text("Заказ на выбранную дату невозможен"));
     }
+
     @Test
     void shouldTestEmptyDate() {
         $("[data-test-id=city] input").setValue("Екатеринбург");
@@ -71,60 +81,63 @@ class OrderingCardDeliveryTest {
         $("[data-test-id=phone] input").setValue("+70000000000");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $("[data-test-id=date] .input_invalid .input__sub").shouldBe(text("Неверно введена дата"));
+        $("[data-test-id=date] .input_invalid .input__sub").shouldHave(text("Неверно введена дата"));
     }
+
     @Test
     void shouldTestNegativeName() {
         $("[data-test-id=city] input").setValue("Екатеринбург");
-        String dateDelivery = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id=date] input").setValue(dateDelivery);
+        String planningDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Ivanov");
         $("[data-test-id=phone] input").setValue("+70000000000");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $("[data-test-id=name].input_invalid .input__sub").shouldBe(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
+
     @Test
     void shouldTestEmptyName() {
         $("[data-test-id=city] input").setValue("Екатеринбург");
-        String dateDelivery = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id=date] input").setValue(dateDelivery);
-        //$("[data-test-id=name] input").setValue("Ivanov");
+        String planningDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=phone] input").setValue("+70000000000");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $("[data-test-id=name].input_invalid .input__sub").shouldBe(text("Поле обязательно для заполнения"));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
     }
+
     @Test
     void shouldTestNegativePhone() {
         $("[data-test-id=city] input").setValue("Екатеринбург");
-        String dateDelivery = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id=date] input").setValue(dateDelivery);
+        String planningDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+799000000000");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $("[data-test-id=phone].input_invalid .input__sub").shouldBe(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
+
     @Test
     void shouldTestEmptyPhone() {
         $("[data-test-id=city] input").setValue("Екатеринбург");
-        String dateDelivery = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id=date] input").setValue(dateDelivery);
+        String planningDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
-        $("[data-test-id=phone].input_invalid .input__sub").shouldBe(text("Поле обязательно для заполнения"));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(text("Поле обязательно для заполнения"));
     }
+
     @Test
     void shouldTestNegativeCheckBox() {
         $("[data-test-id=city] input").setValue("Екатеринбург");
-        String dateDelivery = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id=date] input").setValue(dateDelivery);
+        String planningDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+79900000000");
         $(byText("Забронировать")).click();
-        $("[data-test-id=agreement].input_invalid").shouldBe(visible);
+        $("[data-test-id=agreement].input_invalid").shouldHave(visible);
     }
-
 }
